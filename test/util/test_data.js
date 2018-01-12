56c448bcf5748ba78f6eb904a54a84b9a2aaf976
Jeff Soloshy <jeff.soloshy@getintheloop.ca>
2018-01-12 14:40:37 -0800

    feat(data): Added data.merge

--- test/util/test_data.js
+++ test/util/test_data.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const findDuplicates = require('./../../lib/util/data').findDuplicates;
+const { sort, findDuplicates, merge } = require('./../../lib/util/data');
 
 describe("Testing Data", () => {
   it("Testing Find Duplicates (3)", () => {
@@ -35,5 +35,27 @@ describe("Testing Data", () => {
     expect(duplicates.length).to.equal(2);
     expect(duplicates).to.deep.contain({ id: 1, data: 1 });
     expect(duplicates).to.deep.contain({ id: 1, data: 2 });
+  });
+
+  it("Testing Sort Empty String First", () => {
+    const data = [{
+      user_id: ''
+    }, {
+      user_id: '1'
+    }];
+    const sorted = sort(data, ['user_id']);
+    expect(sorted).to.deep.equal([{ user_id: '' }, { user_id: '1' }]);
+  });
+
+  it("Testing Merge Order", () => {
+    const data = [{
+      id: 1,
+      user_id: ''
+    }, {
+      id: 1,
+      user_id: '1'
+    }];
+    const merged = merge(data, 'id');
+    expect(merged).to.deep.equal([{ id: 1, user_id: '1' }]);
   });
 });
