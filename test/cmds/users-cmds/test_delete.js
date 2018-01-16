c6f0f77ae0c076ff0b215a8e834996b6f77bfb23
Jeff Soloshy <jeff.soloshy@getintheloop.ca>
2018-01-15 18:29:40 -0800

    chore(tests): Extracted file creation/deletion to (before|after)Each

--- test/cmds/users-cmds/test_delete.js
+++ test/cmds/users-cmds/test_delete.js
@@ -5,13 +5,21 @@ const tmp = require('tmp');
 const deleteUsers = require('./../../../lib/cmds/users-cmds/delete');
 
 describe("Testing Users Delete", () => {
+  let tmpFile;
   before(() => {
     nockBack.setMode("record");
     nockBack.fixtures = path.join(__dirname, "__cassette");
   });
 
+  beforeEach(() => {
+    tmpFile = tmp.fileSync();
+  });
+
+  afterEach(() => {
+    tmpFile.removeCallback();
+  });
+
   it("Testing Delete from CSV", (done) => {
-    const tmpFile = tmp.fileSync();
     fs.writeFileSync(tmpFile.name, 'user_id,name\n0123,Test 1\n4567,Test 2');
     nockBack('intercom-bulk-delete-users.json', {}, (nockDone) => {
       deleteUsers.handler({
--- test/cmds/users-cmds/test_update.js
+++ test/cmds/users-cmds/test_update.js
@@ -5,13 +5,21 @@ const tmp = require('tmp');
 const update = require('./../../../lib/cmds/users-cmds/update');
 
 describe("Testing Users Update", () => {
+  let tmpFile;
   before(() => {
     nockBack.setMode("record");
     nockBack.fixtures = path.join(__dirname, "__cassette");
   });
 
+  beforeEach(() => {
+    tmpFile = tmp.fileSync();
+  });
+
+  afterEach(() => {
+    tmpFile.removeCallback();
+  });
+
   it("Testing Update from CSV", (done) => {
-    const tmpFile = tmp.fileSync();
     fs.writeFileSync(tmpFile.name, 'user_id,name\n0123,Test 1\n4567,Test 2');
     nockBack('intercom-bulk-update-users.json', {}, (nockDone) => {
       update.handler({
